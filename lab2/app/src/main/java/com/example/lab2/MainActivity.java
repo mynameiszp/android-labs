edb4b0e2dc42b37d86049b568246ececfcdb1205
Anastasiia Zaporozhchenko <nastyazaporozhchenko@gmail.com>
2024-03-12 21:15:54 +0200

    workable version (not completed)

--- lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,9 +10,16 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity {
+
+    Button OKbutton;
+    ChoiceFragment choiceFragment;
     private RadioGroup radioGroup;
     private RadioButton radioButton;
     private TextView resultText;
@@ -20,21 +28,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        resultText = (TextView)findViewById(R.id.textView4);
-        spinner = findViewById(R.id.spinner);
+        openChoiceMenu();
+        //resultText = (TextView)findViewById(R.id.textView4);
+        /*spinner = choiceFragment.requireView().findViewById(R.id.spinner);
 
         ArrayAdapter<?> adapter =
                 ArrayAdapter.createFromResource(this, R.array.phoneTypes,
                         android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        radioGroup = findViewById(R.id.radioGroup);
-        Button OKbutton = (Button) findViewById(R.id.button);
-        OKbutton.setOnClickListener(this);
+        spinner.setAdapter(adapter);*/
+        //radioGroup = choiceFragment.getView().findViewById(R.id.radioGroup);
+        OKbutton = (Button) findViewById(R.id.button);
+        OKbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResult();
+            }
+        });
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button BackButton = (Button) findViewById(R.id.cancel_button);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToChoiceMenu();
+            }
+        });
     }
 
-    public void onClick(View arg0) {
+    /*public void onClick(View arg0) {
         checkButton(arg0);
         spinner = findViewById(R.id.spinner);
         String phoneType = spinner.getSelectedItem().toString();
@@ -46,15 +66,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String brand = radioButton.getText().toString();
             resultText.setText("Selected: " + brand + ", " + phoneType);
         }
-    }
+        openChoiceMenu();
+    }*/
 
     private void openDialog() {
         WarningDialog warningDialog = new WarningDialog();
         warningDialog.show(getSupportFragmentManager(), "warning dialog");
     }
 
-    private void checkButton(View v){
+    private void checkButton(View v) {
         int id = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(id);
+    }
+
+    private void openChoiceMenu() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        choiceFragment = new ChoiceFragment();
+        fragmentTransaction.replace(R.id.fragment1, choiceFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void openResult() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment1, new ResultFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void backToChoiceMenu() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.popBackStack();
     }
 }
---
+++ lab2/app/src/main/java/com/example/lab2/ResultFragment.java
@@ -0,0 +1,27 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ResultFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ResultFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_result, container, false);
+        return view;
+    }
+}
