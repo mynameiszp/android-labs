c7df806e1cdd2fe2975d4b2b0ee16d283d342937
Anastasiia Zaporozhchenko <nastyazaporozhchenko@gmail.com>
2024-03-12 23:12:56 +0200

    Completed lab2

--- lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.lab2;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,95 +9,106 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button OKbutton;
-    ChoiceFragment choiceFragment;
+    private Button okButton;
+    private ChoiceFragment choiceFragment;
+    private ResultFragment resultFragment;
     private RadioGroup radioGroup;
-    private RadioButton radioButton;
+    private RadioButton modelRadioButton;
     private TextView resultText;
     private Spinner spinner;
+    private String brand;
+    private String phoneType;
+    private Button backButton;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openChoiceMenu();
-        //resultText = (TextView)findViewById(R.id.textView4);
-        /*spinner = choiceFragment.requireView().findViewById(R.id.spinner);
-
-        ArrayAdapter<?> adapter =
-                ArrayAdapter.createFromResource(this, R.array.phoneTypes,
-                        android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);*/
-        //radioGroup = choiceFragment.getView().findViewById(R.id.radioGroup);
-        OKbutton = (Button) findViewById(R.id.button);
-        OKbutton.setOnClickListener(new View.OnClickListener() {
+        choiceFragment = new ChoiceFragment();
+        resultFragment = new ResultFragment();
+        okButton = (Button) findViewById(R.id.button);
+        okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                checkModel(v);
                 openResult();
             }
         });
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button BackButton = (Button) findViewById(R.id.cancel_button);
-        BackButton.setOnClickListener(new View.OnClickListener() {
+        backButton = (Button) findViewById(R.id.cancel_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 backToChoiceMenu();
             }
         });
+        openChoiceMenu();
     }
 
-    /*public void onClick(View arg0) {
-        checkButton(arg0);
-        spinner = findViewById(R.id.spinner);
-        String phoneType = spinner.getSelectedItem().toString();
-        if (radioButton == null){
-            //Toast.makeText(this, "Enter full data", Toast.LENGTH_SHORT).show();
-            openDialog();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (choiceFragment != null && choiceFragment.getView() != null) {
+            spinner = choiceFragment.getView().findViewById(R.id.spinner);
+            ArrayAdapter<?> adapter =
+                    ArrayAdapter.createFromResource(this, R.array.phoneTypes,
+                            android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            radioGroup = choiceFragment.getView().findViewById(R.id.radioGroup);
         }
-        else {
-            String brand = radioButton.getText().toString();
+        if (resultFragment != null && resultFragment.getView() != null) {
+            resultText = (TextView) resultFragment.getView().findViewById(R.id.textView4);
             resultText.setText("Selected: " + brand + ", " + phoneType);
         }
-        openChoiceMenu();
-    }*/
+    }
 
     private void openDialog() {
         WarningDialog warningDialog = new WarningDialog();
         warningDialog.show(getSupportFragmentManager(), "warning dialog");
     }
 
-    private void checkButton(View v) {
+    private void checkModel(View v) {
         int id = radioGroup.getCheckedRadioButtonId();
-        radioButton = findViewById(id);
+        modelRadioButton = findViewById(id);
     }
 
     private void openChoiceMenu() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        choiceFragment = new ChoiceFragment();
         fragmentTransaction.replace(R.id.fragment1, choiceFragment);
-        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        backButton.setVisibility(View.INVISIBLE);
+        okButton.setVisibility(View.VISIBLE);
     }
 
     private void openResult() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.replace(R.id.fragment1, new ResultFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        if (modelRadioButton == null){
+            //Toast.makeText(this, "Enter full data", Toast.LENGTH_SHORT).show();
+            openDialog();
+        }
+        else {
+            phoneType = spinner.getSelectedItem().toString();
+            brand = modelRadioButton.getText().toString();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment1, resultFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            backButton.setVisibility(View.VISIBLE);
+            okButton.setVisibility(View.INVISIBLE);
+            onResume();
+        }
     }
 
     private void backToChoiceMenu() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.popBackStack();
+        backButton.setVisibility(View.INVISIBLE);
+        okButton.setVisibility(View.VISIBLE);
+        onResume();
     }
 }
